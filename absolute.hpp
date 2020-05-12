68ace3497feeaee102fc98311e7933b57aa76a1b
sjj <m18023019495@163.com>
2020-05-12 18:45:27 +0800

    first commit

---
+++ absolute.hpp
@@ -0,0 +1,11 @@
+//return the absolute value of n.
+//for negative n the funciton returns -n, otherwise return n.
+int AbsoluteValue(int n)
+{
+    if(n < 0)
+    {
+        n = -n;
+    }
+    
+    return n;
+}
